497c2734190956d071d40f18b75fd69b3d68da64
Mikolajkusinski <77103765+Mikolajkusinski@users.noreply.github.com>
2022-09-03 09:48:37 +0200

    added styles, dummy cart and dummy buttons

---
+++ src/components/UI/Input.js
@@ -0,0 +1,10 @@
+import styles from "./Input.module.css";
+
+function Input(props) {
+    return<div className={styles.input}>
+        <label htmlFor={props.input.id}>{props.label}</label>
+        <input/>
+    </div>
+}
+
+export default Input;
