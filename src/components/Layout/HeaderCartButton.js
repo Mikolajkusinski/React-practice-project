1c353599e9210ea4707705c55fa83cbedebdabc9
Mikolajkusinski <77103765+Mikolajkusinski@users.noreply.github.com>
2022-09-06 08:28:12 +0200

    added cart module and adding to cart logic

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,24 @@
-import React from "react";
-
+import { useContext } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
-function HeaderCartButton(props){
-    return(<button className={styles.button}>
-        <span className={styles.icon}>
-            <CartIcon/>
-        </span>
-        <span>Your Cart</span>
-        <span className={styles.badge}>
-            3
-        </span>
-    </button>);
+function HeaderCartButton(props) {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  return (
+    <button className={styles.button} onClick={props.onClick}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={styles.badge}>{numberOfItems}</span>
+    </button>
+  );
 }
 
 export default HeaderCartButton;
